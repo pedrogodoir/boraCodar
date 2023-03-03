2d28358abbcdbe31cb3ec6cfcdb34b8352ca0cd4
Pedro Godoi <pedrogodoiriva@gmail.com>
2023-03-03 17:29:48 -0300

    Create of "header"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,19 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.card}></div>
+      <div className={styles.card}>
+          <p className={styles.backCodar}>#boraCodar #boraCodar</p>
+        <div>
+          <img className={styles.perfil_real} src="./perfil_real.png" alt="perfil_real" />
+          <h1 className={styles.perfil}>@pedrogodoi</h1>
+          <img className={styles.perfil_draw} src="./perfil_draw.png" alt="perfil_draw" />
+        </div>
+        <div className={styles.outGrid}>
+          <div>
+
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
