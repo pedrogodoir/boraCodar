11be517c890714c3371ca9e69f362e9d2e7a9dc3
Pedro Godoi <pedrogodoiriva@gmail.com>
2023-03-06 20:15:40 -0300

    Class's 'codar' and txt_perfil upgraded. Added cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import styles from './page.module.css'
+import Link from 'next/link'
+
+
 
 export default function Home() {
+  
   return (
     <main className={styles.main}>
-      <div className={styles.card}>
-          <p className={styles.backCodar}>#boraCodar #boraCodar</p>
-        <div>
-          <img className={styles.perfil_real} src="./perfil_real.png" alt="perfil_real" />
-          <h1 className={styles.perfil}>@pedrogodoi</h1>
-          <img className={styles.perfil_draw} src="./perfil_draw.png" alt="perfil_draw" />
-        </div>
-        <div className={styles.outGrid}>
+      <div className={styles.back}>
+        <div className={styles.out_flex}>
+          <p className={styles.back_codar}>#boraCodar <span className={styles.sec_codar}>#boraCodar</span></p>
           <div>
-
+            <div>
+              <img className={styles.perfil_real} src="./perfil_real.png" alt="perfil_real" />
+              <img className={styles.perfil_draw} src="./perfil_draw.png" alt="perfil_draw" />
+            </div>
+            <a className={styles.txt_perfil} href="https://github.com/pedrogodoir" target='_blank'>@pedrogodoir</a>
           </div>
+        </div>
+        <p>Meus projetos:</p>
+        <div className={styles.out_grid}>
+          <Link href="/conversor-de-moedas">
+            <div className={styles.card}>
+            </div>
+          </Link>
         </div>
       </div>
     </main>
