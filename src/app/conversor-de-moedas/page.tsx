592240376fefb483562d20727cdf1583c686b3e7
Pedro Godoi <pedrogodoiriva@gmail.com>
2023-03-08 16:27:32 -0300

    Adjusting errors

--- src/app/conversor-de-moedas/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-    title : '#boraCodar - Conversor de moedas',
-}
-
-export default function conversorDeMoedas() {
-    <h1>eae</h1>
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,11 @@ export default function Home() {
   
   return (
     <main className={styles.main}>
-      <div className={styles.back}>
         <div className={styles.out_flex}>
           <p className={styles.back_codar}>#boraCodar <span className={styles.sec_codar}>#boraCodar</span></p>
           <div>
             <div>
               <img className={styles.perfil_real} src="./perfil_real.png" alt="perfil_real" />
-              <img className={styles.perfil_draw} src="./perfil_draw.png" alt="perfil_draw" />
             </div>
             <a className={styles.txt_perfil} href="https://github.com/pedrogodoir" target='_blank'>@pedrogodoir</a>
           </div>
@@ -25,7 +23,6 @@ export default function Home() {
             </div>
           </Link>
         </div>
-      </div>
     </main>
   )
 }
