f972f2fe1d569c30ee21f467031543905045e334
Pedro Godoi <pedrogodoiriva@gmail.com>
2023-09-28 11:57:43 -0300

    home frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="grid grid-cols-5 gap-4 m-10">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+    </div>
+</template>
+
+<style>
+.card {
+    background-color: rgb(23 23 23);
+    height: 13rem;
+    border-radius: 0.375rem;
+    transition-duration: 100ms;
+}
+
+.card:hover {
+    transition-duration: 100ms;
+    background-color: rgb(88 28 135);
+    cursor: pointer;
+}
+</style>
